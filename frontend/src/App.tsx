48e4d22fa231bac97aaf69722cbb040354a66956
walid <zarwi.walid@gmail.com>
2024-10-13 11:48:13 +0100

    start new project react vite tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import { Button } from "flowbite-react";
+import "./App.css";
+
+function App() {
+  return <Button className="text-3xl font-bold underline">Hello world!</Button>;
+}
+
+export default App;
